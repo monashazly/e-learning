e7ea5117938dfff700ef2d8af51a23838d8cd311
monashazly <monashazly225@gmail.com>
2022-01-20 14:18:40 +0200

    added env file

--- app/controller/student.controller.js
+++ app/controller/student.controller.js
@@ -1,4 +1,24 @@
+const studentModel=require("../../models/studentmodel")
 class Student {
+    // [register] - [login] - [myCourses] - [profile] - [edit Profile] - [exames] - [add course] - [delete course]
+    static PostRegister=async(req,res)=>{
+       try  {  let student =new studentModel(req.body)
+           await student.save()
+           res.status(200).send({
+               data:student,
+               apistatus:true,
+               message:"data inserted successfully"
+           })
+        }
+           catch(e){
+               res.status(500).send({
+                   apistatus:false,
+                   data:e.message,
+                   message:"error in insertion"
+               })
+
+           }
+    }
    
 
 
