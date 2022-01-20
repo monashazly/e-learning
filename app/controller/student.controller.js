0ba2c64e6ea45e1f8ecf5cc7df2d381a7ee63ef0
monashazly <monashazly225@gmail.com>
2022-01-20 16:56:42 +0200

    edit register login

--- app/controller/student.controller.js
+++ app/controller/student.controller.js
@@ -1,26 +1,54 @@
-const studentModel=require("../../models/studentmodel")
+const studentModel = require('../../models/studentmodel')
+const bycryptjs =require("bcryptjs")
 class Student {
-    // [register] - [login] - [myCourses] - [profile] - [edit Profile] - [exames] - [add course] - [delete course]
-    static PostRegister=async(req,res)=>{
-       try  {  let student =new studentModel(req.body)
-           await student.save()
-           res.status(200).send({
-               data:student,
-               apistatus:true,
-               message:"data inserted successfully"
-           })
-        }
-           catch(e){
-               res.status(500).send({
-                   apistatus:false,
-                   data:e.message,
-                   message:"error in insertion"
-               })
-
-           }
+  //  - [myCourses] - [profile] - [edit Profile] - [exames] - [add course] - [delete course]-show single
+  static PostRegister = async (req, res) => {
+    try {
+      let student = new studentModel(req.body)
+      await student.save()
+      res.status(200).send({
+        data: student,
+        apistatus: true,
+        message: 'data inserted successfully'
+      })
+    } catch (e) {
+      res.status(500).send({
+        apistatus: false,
+        data: e.message,
+        message: 'error in insertion'
+      })
     }
-   
+  }
+  static PostLogin = async (req, res) => {
+    try {
+      let student = await studentModel.login(req.body.email, req.body.password)
+      let token = await student.GenerateToken()
 
-
+      res.status(200).send({
+        apistatus: true,
+        data: { student, token },
+        message: 'logged in successfully'
+      })
+    } catch (e) {
+      res.status(500).send({
+        data: e.message,
+        apistatus: false,
+        message: 'Error'
+      })
+    }
+  }
+  static getEditProfile=async(req,res)=>{
+  try{    const student =await studentModel.findOne({_id:req.params.id})
+      student.name=req.body.name
+ student.password=await bycryptjs.hash(req.body.password,parseInt(process.env.PASSWORDHASH))
+      student.save()
+      res.send({
+          data:student
+      })}
+      catch(e){
+          res.send(e.message)
+      }
+      //upload photo
+  }
 }
-module.exports=Student
+module.exports = Student
--- models/studentmodel.js
+++ models/studentmodel.js
@@ -1,6 +1,7 @@
 const  mongoose=require("mongoose")
 const validator =require("validator")
 const bycryptjs =require("bcryptjs")
+const jwt=require("jsonwebtoken")
 const studentSchema= new mongoose.Schema({
 
     name:{
@@ -29,14 +30,36 @@ const studentSchema= new mongoose.Schema({
             required:true
         },
         grade:{}
+    }],
+    tokens:[{
+        tokens:{type:String,
+        required:true}
     }]
      
 },{tiemstamps:true})
+//passwordhash
 studentSchema.pre("save",async function(){
     const student=this
     if(student.isModified("password"))
       student.password=await bycryptjs.hash(student.password,parseInt(process.env.PASSWORDHASH))
 
 })
+//login function
+studentSchema.statics.login=async function(Email,password){
+const student=await Student.findOne({Email})
+ if(!student)throw new Error("not a user")
+
+ const isCorrect =await bycryptjs.compare(password,student.password)
+ if(!isCorrect) throw new Error("password not valid")
+ return student
+
+}
+//generate token
+studentSchema.methods.GenerateToken=async function(){
+const student =this
+const token= await jwt.sign({_id:student._id},process.env.TOKENHASHSECRET)
+return token
+
+}
 const Student=mongoose.model("Student",studentSchema)
 module.exports=Student
