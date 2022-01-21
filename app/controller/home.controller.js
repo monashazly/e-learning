1de3fc08a16a251e8c13c09339c06544e7d4383c
a-Halawany <eahmad96@icloud.com>
2022-01-22 00:18:41 +0200

    zoom 4 times :)

--- app/controller/home.controller.js
+++ app/controller/home.controller.js
@@ -1,25 +1,53 @@
-const studentModel = require('../../models/studentmodel')
+const studentModel = require('../../models/student.model')
+const teacherModel = require('../../models/teacher.model')
+const resData = require('../helper/resData')
 
 class Home {
+    static postRegister = async (req, res) => {
+        if (req.body.userType == 'student') {
+            try {
+                let student = new studentModel(req.body)
+                await student.save()
+                resData(res, 200, true, student, 'data inserted successfully')
+            } catch (e) {
+                resData(res, 500, false, e.message, 'error in insertion')
+            }
+        } else if (req.body.userType == 'teacher') {
+            try {
+                let teacher = new teacherModel(req.body)
+                await teacher.save()
+                resData(res, 200, true, teacher, 'data inserted successfully')
+            }
+            catch (e) {
+                resData(res, 500, false, e.message, 'error in insertion')
+            }
+        }
+    }
+
     static postLogin = async (req, res) => {
-        if (req.body.status == 'student') {
+        if (req.body.userType == 'student') {
             try {
                 let student = await studentModel.login(req.body.email, req.body.password)
                 let token = await student.GenerateToken()
-                res.status(200).send({
-                    apistatus: true,
-                    data: { student, token },
-                    message: 'logged in successfully'
-                })
+
+                student.tokens.push({ token })
+                await student.save()
+                resData(res, 200, true, student, 'logged in successfully')
             } catch (e) {
-                res.status(500).send({
-                    data: e.message,
-                    apistatus: false,
-                    message: 'Error'
-                })
+                resData(res, 500, false, e.message, 'Error')
             }
-        } else {
-            res.send(req.body)
+
+        } else if (req.body.userType == 'teacher') {
+
+            try {
+                let teacher = await teacherModel.login(req.body.email, req.body.password)
+                let token = await teacher.generateToken()
+                teacher.tokens.push(token)
+                await teacher.save()
+                resData(res, 200, true, teacher, 'logged in successfully')
+            } catch (e) {
+                resData(res, 500, false, e.message, 'Error')
+            }
         }
     }
 }
